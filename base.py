bd92948b56356202fa44620849a27ec9474074c7
Jhonatan Rueda <jhonrue28@gmail.com>
2026-02-13 10:54:39 -0500

    menuPrincipal

---
+++ base.py
@@ -0,0 +1,23 @@
+infoCampers=[{
+    "nombre":0,
+    "apellidos":0,
+    "direccion":0,
+    "acudiente":0,
+    "telCel":0,
+    "TelFijo":0,
+    "Estado":0,
+    "Riesgo":0,
+}]
+
+print ("Menú principal")
+print("¿Quién eres?")
+print("1.Camper")
+print("2.Trainer")
+print("3.Coordinador")
+opcion1=int(input(": "))
+if opcion1==1:
+    print("Eres camper")
+if opcion1==2:
+    print("Eres Trainer")
+if opcion1==3:
+    print("Eres Administrador")
